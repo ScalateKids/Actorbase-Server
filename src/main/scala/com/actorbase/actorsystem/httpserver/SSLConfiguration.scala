e0bc32f0a1b760de0d201445fb3d423a70faee42
Andrea Giacomo Baldan <a.g.baldan@gmail.com>
2016-05-27 11:15:06 +0200

    fix

--- src/main/scala/com/actorbase/actorsystem/httpserver/SSLConfiguration.scala
+++ src/main/scala/com/actorbase/actorsystem/httpserver/SSLConfiguration.scala
@@ -76,9 +76,9 @@ trait SslConfiguration {
     ServerSSLEngineProvider { engine =>
       // engine.setEnabledCipherSuites(Array("TLS_RSA_WITH_AES_256_CBC_SHA"))
       // engine.setEnabledCipherSuites(Array("TLS_RSA_WITH_AES_256_CBC_SHA", "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA", "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA", "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA", "TLS_RSA_WITH_AES_128_GCM_SHA256", "TLS_RSA_WITH_AES_256_CBC_SHA", "TLS_RSA_WITH_AES_128_CBC_SHA", "SSL_RSA_WITH_3DES_EDE_CBC_SHA"))
-      // engine.setEnabledProtocols(Array("TLSv1", "TLSv1.2", "TLSv1.1")) // SSLv3
-      engine.setEnabledCipherSuites(Array("TLS_RSA_WITH_AES_256_CBC_SHA"))
-      engine.setEnabledProtocols(Array("SSLv3", "TLSv1"))
+      engine.setEnabledProtocols(Array("TLSv1", "TLSv1.2", "TLSv1.1")) // SSLv3
+      // engine.setEnabledCipherSuites(Array("TLS_RSA_WITH_AES_256_CBC_SHA"))
+      // engine.setEnabledProtocols(Array("SSLv3", "TLSv1"))
       engine
     }
   }
