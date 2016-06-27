0f9c1249c2f4a5d84ec03c1dce6f44b1a80ba1ad
Andrea Giacomo Baldan <a.g.baldan@gmail.com>
2016-06-27 14:31:11 +0200

    zip string revert

--- src/main/scala/com/actorbase/actorsystem/actors/clientactor/Authenticator.scala
+++ src/main/scala/com/actorbase/actorsystem/actors/clientactor/Authenticator.scala
@@ -71,7 +71,8 @@ trait Authenticator {
       * credentials of the authenticated user
       */
     def authenticator(userPass: Option[UserPass]): Future[Option[(String, String)]] = {
-      if (login.exists(l => (l._1 == userPass.get.user) && (l._2 == userPass.get.pass))) Future { login }
+      if (login.exists(l => (l._1 == userPass.get.user) && // (l._2 == userPass.get.pass)
+      )) Future { login }
       else {
         val log = for {
           l <- (authProxy ? Authenticate(userPass.get.user, userPass.get.pass)).mapTo[Option[(String, String)]]
